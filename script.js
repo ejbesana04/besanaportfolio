100f85536406783ad577a27b743cee135ae8fe7f
ejbesana04 <ej04besana@gmail.com>
2026-01-05 00:28:17 +0800

    update

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuIcon = document.querySelector('#menu-icon');
+const navLinks = document.querySelector('.nav-links');
+
+menuIcon.onclick = () => {
+    navLinks.classList.toggle('active');
+}
+
+function toggleDark() {
+    document.body.classList.toggle("dark");
+
+    const icon = document.querySelector(".theme-toggle");
+
+    if (document.body.classList.contains("dark")) {
+        icon.classList.remove("fa-moon");
+        icon.classList.add("fa-sun");
+    } else {
+        icon.classList.remove("fa-sun");
+        icon.classList.add("fa-moon");
+    }
+}
+
